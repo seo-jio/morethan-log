4643de297e31c3979e0b20d699bbe7725127c06d
seo-jio <81673820+seo-jio@users.noreply.github.com>
2023-08-23 09:28:34 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "SeoJio",
-    image: "https://notion-avatar.vercel.app/api/img/eyJmYWNlIjoxMSwibm9zZSI6MTIsIm1vdXRoIjoxNCwiZXllcyI6MTAsImV5ZWJyb3dzIjoxMywiZ2xhc3NlcyI6MTMsImhhaXIiOjM0LCJhY2Nlc3NvcmllcyI6MCwiZGV0YWlscyI6MCwiYmVhcmQiOjAsImZsaXAiOjAsImNvbG9yIjoicmdiYSgyNTUsIDAsIDAsIDApIiwic2hhcGUiOiJub25lIn0=", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "backend developer",
     bio: "I want to be a backend engineer",
     email: "seojo8625@gmail.com",
@@ -16,11 +16,13 @@ const CONFIG = {
       href: "https://github.com/seo-jio/Pokerface",
       theme: "auto",
     },
+    
     {
       name: `GaVoyage`,
       href: "https://github.com/seo-jio/Gavoyage",
       theme: "auto",
     },
+    
     {
       name: `4can1000won`,
       href: "https://github.com/seo-jio/4can10000won",
@@ -38,7 +40,10 @@ const CONFIG = {
   since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
-
+  seo: {
+    keywords: ["Blog", "Website", "Notion"],
+  },
+  
   // notion configuration (required)
   notionConfig: {
     pageId: process.env.NOTION_PAGE_ID,
